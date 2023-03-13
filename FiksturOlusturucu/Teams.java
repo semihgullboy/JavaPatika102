a1b0e467aef89fd4413cef6312d57830f1e1af1a
Semih <semihgullboy@hotmail.com>
2023-03-13 16:38:45 +0300

    Fikstür

---
+++ FiksturOlusturucu/Teams.java
@@ -0,0 +1,23 @@
+public class Teams implements Comparable<Teams>{
+
+    private String teamName;
+
+    public Teams(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    @Override
+    public int compareTo(Teams o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
